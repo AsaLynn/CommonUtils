7c910ba31204ead5d81dee44d4d4f37e8f185746
zxn <zxn13716071676@sina.com>
2019-03-29 11:02:37 +0800

    增加单位尺寸方法转换.

--- zcommonutils/src/main/java/com/zcommon/lib/UIUtils.java
+++ zcommonutils/src/main/java/com/zcommon/lib/UIUtils.java
@@ -210,6 +210,55 @@ public class UIUtils {
         isNull();
         return mContext.getResources().getDimensionPixelOffset(id);
     }
+
+    /**
+     * dp转px
+     *
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(final float dpValue) {
+        isNull();
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dp
+     *
+     * @param pxValue px值
+     * @return dp值
+     */
+    public static int px2dp( final float pxValue) {
+        isNull();
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(final float spValue) {
+        isNull();
+        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param pxValue px值
+     * @return sp值
+     */
+    public static int px2sp(final float pxValue) {
+        isNull();
+        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
     //-------------------------一下方法为内部使用-----------------------------
 
     private static boolean aboveApiLevel(int sdkInt) {

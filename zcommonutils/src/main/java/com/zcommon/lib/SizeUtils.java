9e664aa2628db7f0d3d8006cfdbdacadf0a92761
zxn <zxn13716071676@sina.com>
2019-03-29 11:53:12 +0800

    增加工具类

---
+++ zcommonutils/src/main/java/com/zcommon/lib/SizeUtils.java
@@ -0,0 +1,54 @@
+package com.zcommon.lib;
+
+import android.content.Context;
+
+/**
+ * 尺寸转换工具类.
+ * Created by zxn on 2019/3/29.
+ */
+public class SizeUtils {
+
+    /**
+     * dp转px
+     *
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(Context context, final float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dp
+     *
+     * @param pxValue px值
+     * @return dp值
+     */
+    public static int px2dp(Context context, final float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context context, final float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param pxValue px值
+     * @return sp值
+     */
+    public static int px2sp(Context context, final float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+}
--- zcommonutils/src/main/java/com/zcommon/lib/UIUtils.java
+++ zcommonutils/src/main/java/com/zcommon/lib/UIUtils.java
@@ -1,5 +1,6 @@
 package com.zcommon.lib;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
  */
 public class UIUtils {
 
+    //-------------------------以下方法需初始化init----------------------------->>>
     private static Context mContext;
 
     /**
@@ -35,28 +37,6 @@ public class UIUtils {
     /**
      * 获取color.xml中的颜色
      *
-     * @param context 上下文
-     * @param id      颜色id,例如R.id.c_ffffff
-     * @return 颜色值
-     */
-    public static int getColor(Context context, @ColorRes int id) {
-        return context.getResources().getColor(id);
-    }
-
-    /**
-     * 获取color.xml中的颜色
-     *
-     * @param view 控件
-     * @param id   颜色id,例如R.id.c_ffffff
-     * @return 颜色值
-     */
-    public static int getColor(View view, @ColorRes int id) {
-        return view.getResources().getColor(id);
-    }
-
-    /**
-     * 获取color.xml中的颜色
-     *
      * @param id 颜色id,例如R.id.c_ffffff
      * @return 颜色值
      */
@@ -64,16 +44,6 @@ public class UIUtils {
         isNull();
 
         return mContext.getResources().getColor(id);
-    }
-
-
-    /**
-     * 获取Context
-     *
-     * @return mContext
-     */
-    public static Context getContext() {
-        return mContext;
     }
 
     /**
@@ -93,6 +63,7 @@ public class UIUtils {
         return mContext.getString(resId, formatArgs);
     }
 
+
     /**
      * Returns a localized string from the application's package's
      * default string table.
@@ -106,61 +77,10 @@ public class UIUtils {
         return mContext.getString(resId);
     }
 
-    /**
-     * 去除字符串空格
-     *
-     * @param text 要处理的字符串.
-     * @return 去除空格后的字符串.
-     */
-    public static String trim(String text) {
-        if (null == text) {
-            return "";
-        }
-        return text.trim();
-    }
-
-    /**
-     * Returns true if the string is null or 0-length or 含有空格视为空串.
-     *
-     * @param str the string to be examined
-     * @return true if str is null or zero length
-     */
-    public static boolean isEmpty(@Nullable CharSequence str) {
-        return str == null || str.length() == 0 || str.toString().trim().length() == 0;
-    }
-
     public static void toast(String msg) {
         isNull();
         ZToastUtils.init(mContext);
         ZToastUtils.toast(msg);
-    }
-
-    /**
-     * 获取屏幕的宽度。
-     *
-     * @param windowManager 窗口管理者。
-     * @return
-     */
-    public static int getScreenWidth(WindowManager windowManager) {
-        int widthPixels = 0;
-        Display defaultDisplay = windowManager.getDefaultDisplay();
-        if (aboveApiLevel(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-                && !aboveApiLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)) {
-            try {
-                widthPixels = (Integer) Display.class.getMethod("getRawWidth").invoke(defaultDisplay);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else if (aboveApiLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)) {
-            android.graphics.Point realSize = new android.graphics.Point();
-            defaultDisplay.getRealSize(realSize);
-            widthPixels = realSize.x;
-        } else {
-            DisplayMetrics metrics = new DisplayMetrics();
-            defaultDisplay.getMetrics(metrics);
-            widthPixels = metrics.widthPixels;
-        }
-        return widthPixels;
     }
 
     /**
@@ -174,19 +94,6 @@ public class UIUtils {
         return getScreenWidth(wmManager);
     }
 
-    /**
-     * 获取dimes.xml中dimen的尺寸值，类似于方法
-     * {@link #getDimension}，不同的是返回值是int类型的值
-     *
-     * @param id 资源id。
-     * @return
-     * @see #getDimension
-     * @see #getDimensionPixelOffset
-     */
-    public static int getDimensionPixelSize(@DimenRes int id) {
-        isNull();
-        return mContext.getResources().getDimensionPixelSize(id);
-    }
 
     /**
      * 获取dimes.xml中dimen的尺寸值。
@@ -219,8 +126,7 @@ public class UIUtils {
      */
     public static int dp2px(final float dpValue) {
         isNull();
-        final float scale = mContext.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+        return dp2px(mContext, dpValue);
     }
 
     /**
@@ -229,10 +135,9 @@ public class UIUtils {
      * @param pxValue px值
      * @return dp值
      */
-    public static int px2dp( final float pxValue) {
+    public static int px2dp(final float pxValue) {
         isNull();
-        final float scale = mContext.getResources().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
+        return px2dp(mContext, pxValue);
     }
 
     /**
@@ -243,8 +148,7 @@ public class UIUtils {
      */
     public static int sp2px(final float spValue) {
         isNull();
-        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (spValue * fontScale + 0.5f);
+        return sp2px(mContext, spValue);
     }
 
     /**
@@ -255,12 +159,153 @@ public class UIUtils {
      */
     public static int px2sp(final float pxValue) {
         isNull();
-        final float fontScale = mContext.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (pxValue / fontScale + 0.5f);
+        return px2sp(mContext, pxValue);
     }
 
-    //-------------------------一下方法为内部使用-----------------------------
+//<<<-------------------------以下方法需初始化init----------------------------|
 
+//|-------------------------完全静态方法,无需初始化init----------------------->>>
+
+    /**
+     * 获取color.xml中的颜色
+     *
+     * @param view 控件
+     * @param id   颜色id,例如R.id.c_ffffff
+     * @return 颜色值
+     */
+    public static int getColor(View view, @ColorRes int id) {
+        return view.getResources().getColor(id);
+    }
+
+    /**
+     * 获取Context
+     *
+     * @return mContext
+     */
+    public static Context getContext() {
+        return mContext;
+    }
+
+    /**
+     * 去除字符串空格
+     *
+     * @param text 要处理的字符串.
+     * @return 去除空格后的字符串.
+     */
+    public static String trim(String text) {
+        if (null == text) {
+            return "";
+        }
+        return text.trim();
+    }
+
+    /**
+     * Returns true if the string is null or 0-length or 含有空格视为空串.
+     *
+     * @param str the string to be examined
+     * @return true if str is null or zero length
+     */
+    public static boolean isEmpty(@Nullable CharSequence str) {
+        return str == null || str.length() == 0 || str.toString().trim().length() == 0;
+    }
+
+    /**
+     * 获取屏幕的宽度。
+     *
+     * @param windowManager 窗口管理者。
+     * @return
+     */
+    public static int getScreenWidth(WindowManager windowManager) {
+        int widthPixels = 0;
+        Display defaultDisplay = windowManager.getDefaultDisplay();
+        if (aboveApiLevel(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+                && !aboveApiLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)) {
+            try {
+                widthPixels = (Integer) Display.class.getMethod("getRawWidth").invoke(defaultDisplay);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (aboveApiLevel(Build.VERSION_CODES.JELLY_BEAN_MR1)) {
+            android.graphics.Point realSize = new android.graphics.Point();
+            defaultDisplay.getRealSize(realSize);
+            widthPixels = realSize.x;
+        } else {
+            DisplayMetrics metrics = new DisplayMetrics();
+            defaultDisplay.getMetrics(metrics);
+            widthPixels = metrics.widthPixels;
+        }
+        return widthPixels;
+    }
+
+    /**
+     * 获取dimes.xml中dimen的尺寸值，类似于方法
+     * {@link #getDimension}，不同的是返回值是int类型的值
+     *
+     * @param id 资源id。
+     * @return
+     * @see #getDimension
+     * @see #getDimensionPixelOffset
+     */
+    public static int getDimensionPixelSize(@DimenRes int id) {
+        isNull();
+        return mContext.getResources().getDimensionPixelSize(id);
+    }
+
+    /**
+     * 获取color.xml中的颜色
+     *
+     * @param context 上下文
+     * @param id      颜色id,例如R.id.c_ffffff
+     * @return 颜色值
+     */
+    public static int getColor(Context context, @ColorRes int id) {
+        return context.getResources().getColor(id);
+    }
+
+    /**
+     * dp转px
+     *
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(Context context, final float dpValue) {
+        return SizeUtils.dp2px(context, dpValue);
+    }
+
+    /**
+     * px转dp
+     *
+     * @param pxValue px值
+     * @return dp值
+     */
+    public static int px2dp(Context context, final float pxValue) {
+        return SizeUtils.px2dp(context, pxValue);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context context, final float spValue) {
+        return SizeUtils.sp2px(context, spValue);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param pxValue px值
+     * @return sp值
+     */
+    public static int px2sp(Context context, final float pxValue) {
+        return SizeUtils.px2sp(context, pxValue);
+    }
+
+//<<<-------------------------完全静态方法,无需初始化init-----------------------
+
+
+    //-------------------------一下方法为内部使用----------------------------->>>
     private static boolean aboveApiLevel(int sdkInt) {
         return getApiLevel() >= sdkInt;
     }
@@ -277,4 +322,6 @@ public class UIUtils {
             throw new RuntimeException("null == mContext,should call init()");
         }
     }
+
+//<<<-------------------------方法为内部使用-----------------------------|
 }

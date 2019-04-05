e0850e30d4cb7d4314da9436150740f78c8faf71
zxn <zhang721588@163.com>
2019-04-05 12:59:32 +0800

    add class used for textview.

--- app/src/main/java/com/zcommon/demo/MainActivity.java
+++ app/src/main/java/com/zcommon/demo/MainActivity.java
@@ -2,21 +2,26 @@ package com.zcommon.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.zcommon.lib.TVUtil;
 import com.zcommon.lib.ZToastUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv_image)
+    TextView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-
+        TVUtil.drawableTop(ivImage, R.mipmap.ic_launcher);
     }
 
     @OnClick(R.id.button)

2f73cd5f7a4d5ce9afe4fbde8b4293a6a1d653ad
zxn721 <zhang721688@163.com>
2019-06-26 16:33:55 +0800

    升级1.2.9:增减软键盘的弹出消失监听

--- app/src/main/java/com/zcommon/demo/MainActivity.java
+++ app/src/main/java/com/zcommon/demo/MainActivity.java
@@ -3,7 +3,9 @@ package com.zcommon.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.zcommon.lib.SoftKeyBoardManager;
 import com.zcommon.lib.TVUtil;
 import com.zcommon.lib.ZToastUtils;
 
@@ -22,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         TVUtil.drawableTop(ivImage, R.mipmap.ic_launcher);
+
+        SoftKeyBoardManager.setKeyBoardListener(this, new SoftKeyBoardManager.OnSoftKeyBoardChangeListener() {
+            @Override
+            public void keyBoardShow(int height) {
+                Toast.makeText(MainActivity.this, "keyBoardShow" + height, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void keyBoardHide(int height) {
+                Toast.makeText(MainActivity.this, "keyBoardHide" + height, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @OnClick(R.id.button)
